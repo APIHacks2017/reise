5294ebe619593654f1936fd73c39e504ae21567a
Hahkarthick <karthick.harish95@gamil.com>
2017-06-17 19:57:44 +0530

    reise files first

--- js/note-list.js
+++ js/note-list.js
@@ -34,7 +34,8 @@
 				url: 'http://52.36.211.72:5555/gateway/WeatherUnderground/v1/forecast10day/q/'+city+'.json',
 				headers: {'x-Gateway-APIKey': '875e02b6-b1c4-496a-b255-847fa5939ada','Accept':'application/json' },
 				success: function(results){
-					console.log(results.forecast.simpleforecast.forecastday.length);
+					//console.log(results.forecast.simpleforecast.forecastday.length);
+					var finalresults="";
 					for(i=0;i<results.forecast.simpleforecast.forecastday.length; i++){
 						var pretty=results.forecast.simpleforecast.forecastday[0].date['pretty'];
 						var highcelsius=results.forecast.simpleforecast.forecastday[0].high['celsius'];
@@ -43,15 +44,31 @@
 						var lowfahrenheit=results.forecast.simpleforecast.forecastday[0].low['fahrenheit'];
 						var icon_url=results.forecast.simpleforecast.forecastday[0].icon_url;
 						var conditions=results.forecast.simpleforecast.forecastday[0].conditions;
-						$('#pretty').text(pretty);
-						$('#highcelsius').text(highcelsius);
-						$('#highfahrenheit').text(highfahrenheit);
-						$('#lowcelsius').text(lowcelsius);
-						$('#lowfahrenheit').text(lowfahrenheit);
-						$('#icon_url').attr('src',icon_url);
-						$('#conditions').text(conditions);
+						//~ $('#pretty').text(pretty);
+						//~ $('#highcelsius').text(highcelsius);
+						//~ $('#highfahrenheit').text(highfahrenheit);
+						//~ $('#lowcelsius').text(lowcelsius);
+						//~ $('#lowfahrenheit').text(lowfahrenheit);
+						//~ $('#icon_url').attr('src',icon_url);
+						//~ $('#conditions').text(conditions);
+						
+						finalresults+="<label>Pretty</label><br/>"+
+									  "<div id='pretty'>"+pretty+"</div><br/>"+
+									  "<label>High Celsius</label><br/>"+
+						"<div id='highcelsius'>"+highcelsius+"</div><br/>"+
+									"<label>High Fahrenheit</label><br/>"+
+						"<div id='highfahrenheit'>"+highfahrenheit+"</div><br/>"+
+									"<label>Low Celsius</label><br/>"+
+						"<div id='locelsius'>"+lowcelsius+"</div><br/>"+
+									"<label>Low Fahrenheit</label><br/>"+
+						"<div id='lowfahrenheit'>"+lowfahrenheit+"</div><br/>"+
+									"<label>Image</label><br/>"+
+						"<div id='icon_url_container'><img src="+icon_url+" id='icon_url' alt=''/></div><br/>"+
+									"<label>Conditions</label><br/>"+
+						"<div id='conditions'>"+conditions+"</div><br/>";
+
 					}
-					
+					$('.sortable').html(finalresults);
 					$('.formcontainer').hide();
 					$('.resultcontainer').show();
 				},
